19286e6d5af25bfc1063108f12976b6b8c68f68f
Ignacio Calvo <ignacio.calvo@gmail.com>
2015-02-27 00:50:43 +0100

    Setup-ConfigFiles.ps1: fixed error

--- tools/Setup-ConfigFiles.ps1
+++ tools/Setup-ConfigFiles.ps1
@@ -5,8 +5,8 @@
     [String] $providerName
 )
 
-.\Update-AppSettings ..\TumblrTools.Viewer\Web.config ConnectionString $Env:ConnectionString
-.\Update-AppSettings ..\TumblrTools.Viewer\Web.config TableName $Env:TableName
-.\Update-AppSettings ..\TumblrTools.Viewer\Web.config PhotosDirectory $Env:PhotosDirectory
-.\Update-AppSettings ..\TumblrTools.Viewer\Web.config ConsumerKey $Env:TumblrConsumerKey
-.\Update-AppSettings ..\TumblrTools.Viewer\Web.config ConsumerSecret $Env:TumblrConsumerSecret
+.\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config ConnectionString $Env:ConnectionString
+.\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config TableName $Env:TableName
+.\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config PhotosDirectory $Env:PhotosDirectory
+.\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config ConsumerKey $Env:TumblrConsumerKey
+.\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config ConsumerSecret $Env:TumblrConsumerSecret
