ae4d9f66e4829703d43dd15cb1976f7969833a78
Ignacio Calvo <ignacio.calvo@gmail.com>
2015-02-27 01:11:48 +0100

    Setup-ConfigFiles.ps1: change to script directory

--- tools/Setup-ConfigFiles.ps1
+++ tools/Setup-ConfigFiles.ps1
@@ -5,6 +5,10 @@
     [String] $providerName
 )
 
+
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+cd $scriptPath
+
 .\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config ConnectionString $Env:ConnectionString
 .\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config TableName $Env:TableName
 .\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config PhotosDirectory $Env:PhotosDirectory
