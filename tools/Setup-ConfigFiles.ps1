0937c20aaef4d657f61a945d66ae286ef50d1ba4
Ignacio Calvo <ignacio.calvo@gmail.com>
2015-03-01 21:07:39 +0100

    Fix

--- tools/Setup-ConfigFiles.ps1
+++ tools/Setup-ConfigFiles.ps1
@@ -16,7 +16,7 @@ mv ..\TumblrTools.Viewer\Web.config.example ..\TumblrTools.Viewer\Web.config
 .\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config ConsumerKey $Env:TumblrConsumerKey
 .\Update-AppSettings.ps1 ..\TumblrTools.Viewer\Web.config ConsumerSecret $Env:TumblrConsumerSecret
 
-mv ..\TumblrTools.CommandLine\Web.config.example ..\TumblrTools.CommandLine\App.config
+mv ..\TumblrTools.CommandLine\App.config.example ..\TumblrTools.CommandLine\App.config
 .\Update-AppSettings.ps1 ..\TumblrTools.CommandLine\App.config ConnectionString $Env:ConnectionString
 .\Update-AppSettings.ps1 ..\TumblrTools.CommandLine\App.config TableName $Env:TableName
 .\Update-AppSettings.ps1 ..\TumblrTools.CommandLine\App.config PhotosDirectory $Env:PhotosDirectory
